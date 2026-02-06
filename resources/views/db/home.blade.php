47cde80eb02d461e81d08ef47301a439af27d3f1
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-02-06 17:09:59 -0500

    Add database layout and views for table selection and display
    - Created a new layout component for database views in `resources/views/components/layout/db.blade.php`.
    - Added a home view for displaying available database tables in `resources/views/db/home.blade.php`.
    - Implemented a table view for displaying rows and columns of a selected table in `resources/views/db/table.blade.php`.
    - Updated routes in `routes/web.php` to handle database home and table views, fetching tables and their respective columns and rows.

---
+++ resources/views/db/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout.db :tables="$tables">
+    <div class="p-4 text-center">
+        Select a table from the list on the left to view its details.
+    </div>
+</x-layout.db>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db', function () {
+    $tables = \DB::connection()->getSchemaBuilder()->getTables();
+
+    return view('db.home', compact('tables'));
+})->name('db.home');
+
+Route::get('/db/table/{table}', function (Illuminate\Http\Request $request, $table) {
+    $tables = \DB::connection()->getSchemaBuilder()->getTables();
+
+    $columns = \DB::connection()->getSchemaBuilder()->getColumnListing($table);
+
+    $currentPage = $request->input('page', 1);
+
+    $rows = \DB::table($table)->paginate(15, ['*'], 'page', $currentPage);
+
+    return view('db.table', compact('tables', 'table', 'columns', 'rows'));
+})->name('db.table');
