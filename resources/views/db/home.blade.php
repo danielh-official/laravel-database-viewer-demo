cf7b3bb2694f5dbbbe75f6a36d15acd37fff73f0
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-02-08 17:28:37 -0500

    Display connection details in a formatted JSON view on the home page

--- resources/views/db/home.blade.php
+++ resources/views/db/home.blade.php
@@ -1,5 +1,12 @@
+@php
+    $connectionDetails = json_encode($connectionDetails, JSON_PRETTY_PRINT);
+    $json = '<pre>' . htmlspecialchars($connectionDetails) . '</pre>';
+@endphp
+
 <x-layout.db :tables="$tables">
-    <div class="p-4 text-center">
-        Select a table from the list on the left to view its details.
+    <div class="p-4 flex flex-col gap-4">
+        <span class="font-mono text-sm text-gray-700 dark:text-gray-300">
+            {!! $json !!}
+        </span>
     </div>
 </x-layout.db>
